dbadd9962fc7d064a3bdb6d3082c680b65745adb
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-20 14:35:03 -0700

    Used Alamofire to download URLS

--- pixel-city/Utilities/Constants.swift
+++ pixel-city/Utilities/Constants.swift
@@ -11,5 +11,6 @@ import Foundation
 let apiKey = "e98f35c5d410ef902a6f6c9c5560f445"
 
 func flickrUrl(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String {
-    return = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+    let url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+    return url
 }
