d38b66a81fa6f33042f667c8e1633d179db7a2db
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-19 18:08:31 -0700

    Added pullUpView and spinner

--- pixel-city/Controllers/MapVC.swift
+++ pixel-city/Controllers/MapVC.swift
@@ -19,6 +19,10 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     var locationManager = CLLocationManager()
     let authorizationStatus  = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
+    var screenSize = UIScreen.main.bounds
+    
+    var spinner : UIActivityIndicatorView?
+    var progressLbl: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,11 +40,33 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         mapView.addGestureRecognizer(doubleTap)
     }
     
+    func addSwipe() {
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+        swipe.direction = .down
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
     func animateViewUp() {
         pullUpViewHeightConstraint.constant = 300
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
         }
+    }
+    
+    @objc func animateViewDown() {
+        pullUpViewHeightConstraint.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func addSpinner() {
+        spinner = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+        spinner?.activityIndicatorViewStyle = .whiteLarge
+        spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        spinner?.startAnimating()
+        pullUpView.addSubview(spinner!)
     }
 
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
@@ -73,6 +99,8 @@ extension MapVC: MKMapViewDelegate {
         print("Pin dropped...")
         removePin()
         animateViewUp()
+        addSwipe()
+        addSpinner()
         
         let touchPoint =  sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
