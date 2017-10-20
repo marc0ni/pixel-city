0b3ed38635b9696e15fffba5773cb93aabc16cd6
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-19 19:47:55 -0700

    Setting actual current location won't work in simulator (no GPS )

--- pixel-city/Controllers/MapVC.swift
+++ pixel-city/Controllers/MapVC.swift
@@ -30,6 +30,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         mapView.showsUserLocation = true
         locationManager.delegate = self
         configureLocationServices()
+        determineCurrentLocation()
         addDoubleTap()
     }
     
@@ -126,6 +127,32 @@ extension MapVC: CLLocationManagerDelegate {
         } else {
             return
         }
+    }
+    
+    func determineCurrentLocation()
+    {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            //locationManager.startUpdatingHeading()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        //manager.stopUpdatingLocation()
+        
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
